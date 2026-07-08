c3eed82a48787d78d65617bbcf14adcd7277d31f
NeoAxiøm <shouryasrivastava2011@gmail.com>
2026-07-08 22:16:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btn");
+const status = document.getElementById("status");
+const fuel = document.getElementById("fuel");
+const speed = document.getElementById("speed");
+const alt = document.getElementById("altitude");
+
+btn.addEventListeners("click",() => {
+  status.textContent("Ignition ON!");
+  btn.disabled = true;
+});
+setTimeout(() => {
+  status.textContent("Launched");
+  for(let i=100,i>0,i--){
+    fuel.textContent("FUEL:"+i+"%")
+    setTimeout(() => {
+    },1000);
+  }
+},5000);
